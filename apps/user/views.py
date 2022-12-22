e8e9cd0161f98340c88d4a33c636abf52b66c1f4
vladislavshokorov <valsshokorov@gmail.com>
2022-12-22 12:52:47 +0600

    Add stub api_view()

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,1 +1,8 @@
-from . import services
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["POST"])
+def login(request):
+    # do something
+    return Response({"message": "login success"})
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from apps.user import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/login/", views.login),
 ]
