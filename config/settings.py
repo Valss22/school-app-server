12f6e693119661ee03646dff2cb63eaeda2ef101
vladislavshokorov <valsshokorov@gmail.com>
2022-12-22 13:37:05 +0600

    Add session auth

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,16 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
