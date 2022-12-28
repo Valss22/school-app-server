1b8f136c5a85aba04a360cb9f4f239af77e820a6
vladislavshokorov <valsshokorov@gmail.com>
2022-12-28 14:22:41 +0600

    Upgrade validation schema typecheking

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
+from rest_framework.response import Response
 from . import services
 
 
@@ -9,8 +10,20 @@ def request_validator(schema: dict):
             for (schema_key, schema_type_key), (data_key, data_value) in zip(
                 schema.items(), request.data.items()
             ):
-                if schema_key != data_key or schema_type_key is not type(data_value):
-                    raise TypeError
+                if schema_key != data_key:
+                    return Response(
+                        {
+                            "msg": "Request body validation error, "
+                            + f"expected {schema_key}, but recieved {data_key}"
+                        }
+                    )
+                if schema_type_key is not type(data_value):
+                    return Response(
+                        {
+                            "msg": "Request body validation error, "
+                            + f"expected {schema_type_key}, but recieved {type(data_value)}"
+                        }
+                    )
             return func(request)
 
         return wrapper
